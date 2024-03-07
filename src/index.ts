75088db3356aadbf1cf91288b1943d18951da2fd
Gurpreetk9 <92116791+Gurpreetk9@users.noreply.github.com>
2024-03-07 21:25:08 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("Connected to database"));
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/health", async (req: Request, res: Response) => {
+  res.send("Health Ok!");
+});
+app.use("/api/my/user", userRoute);
+
+app.listen(7000, () => {
+  console.log("Server is running at port 7000");
+});
---
+++ src/routes/userRoute.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import userController from "../controllers/userController";
+import { jwtCheck, jwtParse } from "../middlewares/auth";
+import { validateUserRequest } from "../middlewares/validation";
+
+const router = express.Router();
+
+router.get("/", jwtCheck, jwtParse, userController.getCurrentUser);
+router.post("/", jwtCheck, userController.createCurrentUser);
+router.put(
+  "/",
+  jwtCheck,
+  jwtParse,
+  validateUserRequest,
+  userController.updateCurrentUser
+);
+export default router;
